7a1306a2ad0a820cbd507a709556ccb4b59c6d9d
angel.aguirre <angelaguirrel@hotmail.com>
2020-05-03 17:42:12 -0500

    The model Post was completed

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+# My app
+from . models import Posts
+
 # Register your models here.
+class PostsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date_created')
+    readonly_fields = ('date_created', 'date_modified')
+
+admin.site.register(Posts, PostsAdmin)
--- src/blog/models.py
+++ src/blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Posts(models.Model):
+    """
+    This class will handle the posts information.
+    The fields in this class:
+    - Title
+    - COntent
+    - Date of creationg
+    - Date for modification
+    """
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'post'
+        verbose_name_plural = 'posts'
